1320aba649560ab8c1545edb191183f675760a74
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2022-01-21 21:08:31 +0200

    Initial Commit

---
+++ DetectingAppStateInSwiftUI/DetectingAppStateInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetectingAppStateInSwiftUIApp.swift
+//  DetectingAppStateInSwiftUI
+//
+//  Created by Ramill Ibragimov on 21.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct DetectingAppStateInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
