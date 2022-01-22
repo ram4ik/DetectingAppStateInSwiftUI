3bba672e93ba433458fefd479a7c19300a76d7c9
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2022-01-22 20:59:41 +0200

    scenePhase

--- DetectingAppStateInSwiftUI/DetectingAppStateInSwiftUIApp.swift
+++ DetectingAppStateInSwiftUI/DetectingAppStateInSwiftUIApp.swift
@@ -6,12 +6,39 @@
 //
 
 import SwiftUI
+import Foundation
 
 @main
 struct DetectingAppStateInSwiftUIApp: App {
+    
+    @Environment(\.scenePhase) var phase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onChange(of: phase) { newPhase in
+                    switch newPhase {
+                    case .active:
+                        print("App in active state")
+                    case .background:
+                        print("App is in background")
+                    case .inactive:
+                        print("App is in inactive state")
+                    @unknown default:
+                        print("Unknown")
+                    }
+                    
+                }
+            
+            
+            
+            
+//                .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+//                    print("App in active state")
+//                }
+//                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification )) { _ in
+//                    print("App is in background")
+//                }
         }
     }
 }
